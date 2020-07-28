7eb9f8d60663ddf64fbb20896eceb06931beca71
R Murakami <travelgo2525@gmail.com>
2020-07-28 21:40:26 +0900

    40nd commit

--- vue-cli-project/src/components/recipe.vue
+++ vue-cli-project/src/components/recipe.vue
@@ -6,16 +6,16 @@
     <div>
         <p class="mainVisual"><img src="../assets/mainvisual.jpg" alt="mainVisual" ></p> 
         <h2>{{title.length>0?title[0]:"カテゴリー未設定"}}</h2>
-        <p>{{point}}</p>
-        <p>{{time}}</p>
+        <!-- <p>{{point}}</p>
+        <p>{{time}}</p> -->
         <h2>材料</h2>
         <ul>
-            <li v-for="item in list" v-bind:key="item.id">
+            <li v-for="item in material" v-bind:key="item.id">
                 {{item.material}} <span>{{item.amount}}</span></li>
         </ul>
-        <h3>作り方</h3>
+        <h3>作り方</h3><p>{{items['name']}}</p>
         <ol>
-            <li v-for="item in list_cook" v-bind:key="item.id">
+            <li v-for="item in recipe" v-bind:key="item.id">
                 {{item.explain}} </li>
         </ol>
 
@@ -40,24 +40,25 @@ export default{
 
             // items:[],
             title:[],
+            items:[],
             
 
             // make:"タコ飯",
-            point:`初めにタコのやわらか煮をつくり、その煮汁とタコを使用します。お好みでホタテやエビなどの魚介類を入れてもおいしいです。バターを足すことでコクを増し、止まらないほどのおいしさに！`,
-            time:"80分（タコのやわらか煮:50分、タコ飯:25分）",
-            list:[//7/23疑問　どうやってfirestoreに入れる？name同じだけど
-                {id:1,material:'タコ（刺身用可', amount:'200~300g程'},
-                {id:2,material:'昆布', amount:'2~3枚'},
-                {id:3,material:'醤油', amount:'大さじ3杯'},
-                {id:4,material:'砂糖', amount:'大さじ2杯'},
-                {id:5,material:'酒', amount:'50ml'},
-                {id:6,material:'水', amount:'350ml'},
-                {id:7,material:'米', amount:'2,5ごう'},
-                {id:8,material:'油揚げ', amount:'2枚'},
-                {id:9,material:'油', amount:'小さじ1杯'},
-                {id:10,material:'バター', amount:'20g(適量)'},
+            // point:`初めにタコのやわらか煮をつくり、その煮汁とタコを使用します。お好みでホタテやエビなどの魚介類を入れてもおいしいです。バターを足すことでコクを増し、止まらないほどのおいしさに！`,
+            // time:"80分（タコのやわらか煮:50分、タコ飯:25分）",
+            material:[//7/23疑問　どうやってfirestoreに入れる？name同じだけど
+                // {id:1,material:'タコ（刺身用可', amount:'200~300g程'},
+                // {id:2,material:'昆布', amount:'2~3枚'},
+                // {id:3,material:'醤油', amount:'大さじ3杯'},
+                // {id:4,material:'砂糖', amount:'大さじ2杯'},
+                // {id:5,material:'酒', amount:'50ml'},
+                // {id:6,material:'水', amount:'350ml'},
+                // {id:7,material:'米', amount:'2,5ごう'},
+                // {id:8,material:'油揚げ', amount:'2枚'},
+                // {id:9,material:'油', amount:'小さじ1杯'},
+                // {id:10,material:'バター', amount:'20g(適量)'},
                 ],
-            list_cook:[
+            recipe:[
                 {id:1, explain:'タコの足を1本ずつ切り分ける（はさみが楽です）'},
                 {id:2, explain:'タコと2~5の材料を加えて、沸騰するまで強火で煮る、沸騰したら弱火で50分蓋をし煮る'},
                 {id:3, explain:'タコを煮て30分したら米を洗い、そのまま水につける（15~20分程）'},
@@ -87,21 +88,25 @@ export default{
 
      var subparams=SLUG_RECIPE_TABLE[params]
      console.log(subparams)
-     this.title.push(subparams) 
+     this.title.push(subparams) //path=octopasrice=タコライスとなる
     
 
-    //   db.collection('items') 
+
+    //recipe_slug", "==",paramsのもののみ取得
+       db.collection('items') 
        
-    //   .where("slug", "==",params)
-    //    .get()  
-    //    .then(snapshot => {  
-    //      snapshot.forEach(doc => {  
-    //        let item = doc.data()  //item
-    //        item.id = doc.id  //item
-    //        console.log("item",item)
-    //        this.items.push(item)  
-    //      })  
-    //    })  
+       .where("recipe_slug", "==",params)
+        .get()  
+        .then(snapshot => {  
+          snapshot.forEach(doc => {  
+          let item = doc.data()  //item
+            item.id = doc.id  //item
+            console.log("item",item)
+            this.items.push(item)  
+            // console.log(items)
+          })  
+       })  
+    //    console.log(items['name'])
 
         
     }
